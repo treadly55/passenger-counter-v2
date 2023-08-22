9587732b5d9543541854e690f3b2920c1e5ecfce
chris wild <chriswild55@gmail.com>
2023-08-23 00:48:19 +1000

    scoreboard working
    functions work
    styling required

--- index.js
+++ index.js
@@ -4,11 +4,17 @@
 
 //buttons need to be in a separate row so that they don't adjust position when then scoreboard extends to 3 values in width
 
-let sumEl = document.getElementById("sumHomevalue")
 
+//assign variables to html elements//
+let sumEl = document.getElementById("sumHomevalue")
+let sumElaway = document.getElementById("sumAwayvalue")
+
+//declare variables//
 let homeScore = 0
 let awayScore = 0
 
+
+//functions for away scoreboard //
 function addOnehome () {
 homeScore += 1
 sumEl.innerText = homeScore
@@ -24,5 +30,18 @@ homeScore += 3
 sumEl.innerText = homeScore
     }
 
+//functions for away scoreboard //
+function addOneAway () {
+awayScore += 1
+sumElaway.innerText = awayScore
+    }
 
+function addTwoAway () {
+awayScore += 2
+sumElaway.innerText = awayScore
+    } 
 
+function addThreeAway () {
+awayScore += 3
+sumElaway.innerText = awayScore
+    }
