aebf8c328e879221396ae5f895ee6eb6c90561a6
chris wild <chriswild55@gmail.com>
2023-08-23 00:00:51 +1000

    scoreboard functionality
    buttons work and add values
    kept align

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 //here we go
+
+//you have to declare things as a variable here and then use them in calculations so that they can return as a integer
+
+//buttons need to be in a separate row so that they don't adjust position when then scoreboard extends to 3 values in width
+
+let sumEl = document.getElementById("sumHomevalue")
+
+let homeScore = 0
+let awayScore = 0
+
+function addOnehome () {
+homeScore += 1
+sumEl.innerText = homeScore
+    }
+
+function addTwohome () {
+homeScore += 2
+sumEl.innerText = homeScore
+    }
+
+function addThreehome () {
+homeScore += 3
+sumEl.innerText = homeScore
+    }
+
+
+
